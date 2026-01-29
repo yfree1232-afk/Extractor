9a0ec5e831fbf9755cda0036d41e394c46c40ad1
adityass07 <ytrial677@gmail.com>
2026-01-29 18:41:26 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# Extractor
+[![Deploy To Heroku](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy?template=https://github.com/Pod94/password)
+
+[![Deploy To Heroku](https://www.herokucdn.com/deploy/button.svg)](https://dashboard.heroku.com/new?template=https://github.com/ifsabhi64/AahuExtractor)
+                     
---
+++ config.py
@@ -0,0 +1,51 @@
+import os
+from os import getenv
+
+
+# ------------------------------------------------
+API_ID = int(os.environ.get("API_ID", "20567114"))
+# ------------------------------------------------
+API_HASH = os.environ.get("API_HASH","8a5b92106e45fc6637a65a67df060a65")
+# ------------------------------------------------
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8024972578:AAFahP8njNmQjN7Xywyo8BQ9Z_Q-232K7CI")
+# ------------------------------------------------
+BOT_USERNAME = os.environ.get("BOT_USERNAME", "@AshuExtractorBot")
+BOT_TEXT = "@AshuExtractorBot"
+# ------------------------------------------------
+OWNER_ID = int(os.environ.get("OWNER_ID", "8036182138"))
+# ------------------------------------------------
+# //LOG CHANNEL ID 
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002829360841"))
+
+# //FORCE_CHANNEL_ID
+CHANNEL_ID2 = int(os.environ.get("CHANNEL_ID2", "-1002829360841")")) 
+# ------------------------------------------------
+MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://vikassonawale0:JWyQFas7vlG1bkaL@cluster0.beermge.mongodb.net/?retryWrites=true&w=majority")
+# -----------------------------------------------
+PREMIUM_LOGS = int(os.environ.get("PREMIUM_LOGS", "-1002829360841"))
+# -----------------------------------------------
+join = '<a href="https://t.me/ifsashu1">✳️ JOIN BACKUP</a>'
+# -----------------------------------------------
+UNSPLASH_ACCESS_KEY = 'RabDRmuXXBobanmwwbvpP5LwoG4J8ox34y5Sstz-9jk'
+# -----------------------------------------------
+UNSPLASH_QUERY = 'animal baby'
+# -----------------------------------------------
+ADMIN_BOT_USERNAME = "AshuExtractorBot" #without @
+
+THUMB_URL = os.environ.get("THUMB_URL", "https://graph.org/file/2a5cb7bb0144ccfda3d37-60007d94009e55415f.jpg")
+
+
+
+
+# # Bot configuration
+# API_ID = int(os.environ.get("API_ID", "20567114"))
+# API_HASH = os.environ.get("API_HASH", "8090907579:AAE5bN3xZmCbuuj9-NH_IEbWjVpq6awfFrk")
+# BOT_TOKEN = os.environ.get("BOT_TOKEN", "8090907579:AAE5bN3xZmCbuuj9-NH_IEbWjVpq6awfFrk")
+# BOT_USERNAME = os.environ.get("BOT_USERNAME", "AbhiTXT1bot")
+# OWNER_ID = int(os.environ.get("OWNER_ID", "8036182138"))
+# SUDO_USERS = list(map(int, getenv("SUDO_USERS", "8036182138").split()))
+# CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002441127578"))
+# MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://wadiro6523:08AwfhhKRdQaS1i6@cluster0.krzxuop.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+# PREMIUM_LOGS = int(os.environ.get("PREMIUM_LOGS", "-1002402673518"))
+# THUMB_URL = os.environ.get("THUMB_URL", "https://graph.org/file/2a5cb7bb0144ccfda3d37-60007d94009e55415f.jpg")
+
